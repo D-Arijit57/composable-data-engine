44c724e55baa98ae4be26ad4d921b3ef733bcbbe
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-20 02:50:10 +0530

    added : models for content_type and organization + migrated

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# Organization
+# models.model is the base class for django for database_backed models
+# interpretation - Organization is a database entity. Take the fields I define inside this class and use them to construct/manage the corresponding database table.
+# django fields are non-null by default
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- content/models.py
+++ content/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import Organization
+
+
+class ContentType(models.Model):
+    # organization represents the relatioship in Django's object model here, while organization_id is the physical foreign key column django creates in the relational database
+    # so doing content.organization_id returns you the primary key value instead of the related object
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name="content_types",
+    )
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["organization", "slug"],
+                name="unique_organization_slug",
+            ),
+        ]
+
+    def __str__(self):
+        return self.name
